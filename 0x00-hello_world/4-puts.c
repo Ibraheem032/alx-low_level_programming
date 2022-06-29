dfd9fdb31d90600f22637e17e480ff34b7016bb6
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-06-29 13:39:44 -0700

    Printing with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() /*Initialising main funtion*/
+int main(void) /*Initialising main funtion*/
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
-
-}	
+}
