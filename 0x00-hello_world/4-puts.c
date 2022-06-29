c571adb1a47757cc05567c19c440177e18cb4546
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-06-29 13:16:02 -0700

    Printing with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,7 @@
 
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
-
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
-
-	return 0;
+	return (0);
 }	
