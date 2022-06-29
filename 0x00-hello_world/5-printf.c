81688a1a97b451066fda3bafdf8e21447939495d
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-06-29 14:37:25 -0700

    Using puts function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>`
+
+/**
+ * main - print string
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	printf("wth proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
