d6dd79d4ea0d836c9c7a9c68d5bf5efcc1a09b90
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-06-29 23:43:17 -0700

    Printing strings

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int a = sizeof(char);
-int b = sizeof(int);
-int c = sizeof(long int);
-int d = sizeof(long long int);
-int e = sizeof(float);
+int char_size = sizeof(char);
+int int_size = sizeof(int);
+int long_int_size = sizeof(long int);
+int long_long_int_size = sizeof(long long int);
+int float_size = sizeof(float);
 
 /**
  * main - prints size of various types on the computer
@@ -13,11 +13,11 @@ int e = sizeof(float);
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", a);
-	printf("Size of an int: %i byte(s)\n", b);
-	printf("Size of a long int: %i byte(s)\n", c);
-	printf("Size of a long long int: %i byte(s)\n", d);
-	printf("Size of a float: %i byte(s)\n", e);
+	printf("Size of a char: %i byte(s)\n", char_size);
+	printf("Size of an int: %i byte(s)\n", int_size);
+	printf("Size of a long int: %i byte(s)\n", long_int_size);
+	printf("Size of a long long int: %i byte(s)\n", long_long_int_size);
+	printf("Size of a float: %i byte(s)\n", float_size);
 
 	return (0);
 }
