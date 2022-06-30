3dc19156484bf94f0471e28e74ac41ea1dd2c07f
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-06-30 00:28:36 -0700

    Printing strings

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints an array of characters
+ *
+ * Return: returns 1
+ */
+int main(int)
+{
+	fprintf(stderr, "and that piece of art is useful\" -Dora Korpar 2015-10-19");
+
+	return (1);
+}	
