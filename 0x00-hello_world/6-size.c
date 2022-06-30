78c07ac115ad72f663e68940b3e81c96cc69031f
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-06-30 00:02:20 -0700

    Printing strings

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-int cz = sizeof(char);
-int iz = sizeof(int);
-int liz = sizeof(long int);
-int lliz = sizeof(long long int);
-int fz = sizeof(float);
 
 /**
  * main - prints size of various types on the computer
@@ -13,6 +8,12 @@ int fz = sizeof(float);
  */
 int main(void)
 {
+int cz = sizeof(char);
+int iz = sizeof(int);
+int liz = sizeof(long int);
+int lliz = sizeof(long long int);
+int fz = sizeof(float);
+
 	printf("Size of a char: %i byte(s)\n", cz);
 	printf("Size of an int: %i byte(s)\n", iz);
 	printf("Size of a long int: %i byte(s)\n", liz);
