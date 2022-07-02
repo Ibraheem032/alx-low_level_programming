3fd21ba2d6cab453fd64612325f4d8e1f30cfe4e
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-01 20:17:35 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	int i;
+	int num;
 
-	for(i = 0; i <= 9; i++)
+	for (num = 10; num <= 19; num++)
+	{	
 		putchar((i % 10) + '0');
 		putchar(',');
 		putchar(' ');
+	}	
 	putchar('\n');
 
 	return (0);
