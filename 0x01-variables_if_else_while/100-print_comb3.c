f5762e8eed3b43fcac0b4e49ab08927b9063e40a
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-03 04:53:15 -0700

    Adding other programs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,23 @@ int main(void)
 	int j;
 
 	for(i = 0; i <= 9; i++)
+	{
 		for(j = 0; j <= 9; j++)
+		{
 			if (i == j)
-			       ;
-			else
-				putchar('int(i)');
-				putchar('int(j)');
-				putchar(',');
-				putchar(' ');
+			      continue;
+
+
+			putchar(i % 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i == 8 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 
 	return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (num = 10; num < 20; num++)
 	{
-		if (num < 19)
-		{
-			putchar((num % 10) + '0');
-			putchar(',');
-			putchar(' ');
-		}
-		else
-			putchar((num % 10) + '0');
+		putchar((num % 10) + '0');
+
+		if (num == 19)
+			continue;
+
+
+		putchar(',');
+		putchar(' ');
 	}
-	'\n';
+	putchar('\n')
 
 	return (0);
 }
