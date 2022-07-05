14d77dec792c977e7d6d7b85a361a9ed5a95861b
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-05 11:10:31 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s[9] = "_putchar"
+	int i;
+
+	for (i = 0, i < 9, i++)
+		_putchar(i);
+
+	_putchar('\n);
+
+	return (0);
+}	
