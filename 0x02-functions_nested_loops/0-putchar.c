c4f9c8c0837ab8f3f01d14234d7e44888ee95a7d
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-05 00:32:13 -0700

    Using putchar to print string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints '_putchar' followed by new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *s[] = {"_putchar"};
+
+	for (i = 0; i in s[]; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+
+}
