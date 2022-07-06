6f0ef47c4cf07c75e848638d04cd07e5d18e7458
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-06 06:44:32 -0700

    Checks for alphabetic characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character
+ * @c: character to input
  *
  * Return: 1 if c is lowercase
  * 0 if otherwise
  */
 int _islower(int c)
 {
+	int i = 0;
+	int j = 1;
 
 	if (c >= 'a' && c <= 'z')
-		return(_putchar(1 % 10 + '0'));
+		return (_putchar(j % 10 + '0'));
 
 	else
-		return(_putchar(10 % 10 + '0'));
+		return (_putchar(i % 10 + '0'));
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
  * _isalpha - Checks for alphabetic characters
+ * @c: character to input
  *
  * Return: 1 if c is an alphabbet
  * 0 if otherwise
  */
 int _isalpha(int c)
 {
+	int i = 1;
+	int j = 0;
+
 	if (c >= 65 && c <= 122)
-		return(_putchar(1 % 10 + '0'));
+		return (_putchar(i % 10 + '0'));
 
 	else
-		return(_putchar(10 % 10 + '0'));
-
-	_putchar('\n');
+		return (_putchar(j % 10 + '0'));
 }
