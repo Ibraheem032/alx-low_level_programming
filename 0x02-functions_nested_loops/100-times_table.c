b855f13e1326dc0c6979f5d07b94df462f9edaf0
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-06 16:51:25 -0700

    Prints times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,12 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			k = i * j;
-			printf("%4d", k);
+			if (j == 0)
+				printf("%d", k);
+
+			else
+				printf("%4d", k);
+
 			if (j == n)
 			{
 				continue;
