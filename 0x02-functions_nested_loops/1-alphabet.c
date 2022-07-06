dc03b3ae7f3327dbe9b465edec0a8dd9af578243
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-06 03:07:19 -0700

    Fixing header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-void print_alphabet(void);
 /**
  * main - Print alphabets
  *
@@ -8,7 +7,12 @@ void print_alphabet(void);
  */
 int main(void)
 {
-	print_alphabet();
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
 
 	return (0);
 }
