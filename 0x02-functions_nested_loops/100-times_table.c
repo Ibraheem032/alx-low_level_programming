791555a7d1c3f5d65c242865cb5cfe2a680b6dfd
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-06 15:15:52 -0700

    Prints natural numbers up to 98

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,13 +10,15 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i <= n; i++)
 		for (j = 0; j <= n; j++)
 		{
-			printf("%i * %i", i, j);
+			k = i * j;
+			printf("%i", k);
 			printf(", ");
 		}
 
-	printf("\n");
+		printf("\n");
 }
