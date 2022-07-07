7e0864237854d8c53c2a09efd54f5db19ac8befb
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-07 02:24:22 -0700

    Adding mul function and readme

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @a: First input  argument
+ * @b: Second input argument
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	printf("%d", product);
+
+	return (0);
+}
