c680afbde392bb8e9728bc777f7b01381bd70207
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-06 20:08:52 -0700

    Prints result of sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Prints result of sum
+ * @int: first argument
+ * @int: second argument
+ *
+ * Return: int
+ */
+int add(int, int)
+{
+	int i;
+	int j;
+	int k = i + j;
+
+	return (printf("%d", k));
+
+}
