e1c19a01a9eefed26e13c35a870ea8402a56e0b4
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-12 07:50:11 -0700

    More commits

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,11 @@
 #include "main.h"
 /**
  * swap_int - swaps two integers
- * @
+ * @*a: input
+ * @*b: input
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Returns string length
+ * @*s: input
+ *
+ * Return: returns string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
