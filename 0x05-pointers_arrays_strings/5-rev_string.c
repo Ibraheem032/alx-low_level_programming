7f4e6861c5f6a2b7775090875d10f98825c2ff18
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-12 20:23:10 -0700

    More programs

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,7 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	int len = strlen(s);
 
-	for (i = 0; i <= len; i++)
-	{
-		s[i] = s[len - i];
-		if (s[i] == '\0')
-			continue;
-		else
-			printf("%c", s[i]);
-	}
+	strrev(s);
 	printf("\n");
 }
