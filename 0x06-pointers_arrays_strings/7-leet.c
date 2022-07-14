f4569ed52fc814998f18e488a47197a2e3a918fc
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-14 10:55:30 -0700

    More programs

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,4 +2,15 @@
 #include "main.h"
 /**
  * leet - encodes a string into 1337
- * @
+ * @s: input string
+ * Return: encoded input string
+ */
+char *leet(char *s)
+{
+	int i;
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		if (
+	}
+}
