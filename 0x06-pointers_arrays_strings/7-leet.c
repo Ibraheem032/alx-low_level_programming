f9eabf09f1d5f8a0276f596ee8ea9430173c012b
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-15 01:41:52 -0700

    more programs

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 /**
  * leet - encodes a string into 1337
  * @s: input string
@@ -10,7 +12,7 @@ char *leet(char *s)
 	int i;
 	int j;
 	char t1[] = "AELOT";
-	int t2[] = "43107";
+	char t2[] = "43107";
 	char t3[] = "aelot";
 
 	for(i = 0; s[i] != '\0'; i++)
@@ -20,6 +22,7 @@ char *leet(char *s)
 			if (s[i] == t1[j] || s[i] == t3[j])
 			{
 				s[i] = t2[j];
+				break;
 			}
 		}
 		printf("%c", s[i]);
