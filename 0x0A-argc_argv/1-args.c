72af5cc568489851f137b1029f94b3d98ce958c4
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-22 14:46:27 -0700

    correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: Pointer to array of string
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 	printf("%i\n", argc - 1);
 	return (0);
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,7 @@
  * main - Prints the product of two numbers
  * @argc: size of array of *argv
  * @argv: Pointer to array of string
- * Return: 1 if argc is not equal to 2, and prints error, else 0
+ * Return: 1 if argc is not equal to 3, else 0
  */
 int main(int argc, char *argv[])
 {
