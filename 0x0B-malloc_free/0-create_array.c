819e2c3f628be4efc787de9439a3ef8669697438
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-26 04:29:08 -0700

    Adding malloc programs

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * create_array - creates an array of chars, and initialisesit with a specific char
+ * @size: size of array to be created
+ * @c: initial character of the array
+ * Return: NULL if size equals zero or if it fails, else, pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	char *s;
+
+	s = malloc(size);
+	s[0] = c;
+	free(s)
+}
