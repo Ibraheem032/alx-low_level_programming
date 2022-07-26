e822bc14356d71f161fde5033e81b5733947322d
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-26 05:18:37 -0700

    Adding malloc programs

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	s = malloc(size);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = c;
-	free(s);
 	return (s);
 
 }
