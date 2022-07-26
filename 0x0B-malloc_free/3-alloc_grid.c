1542d2bfc958b9ac75c34258de826b73851017be
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-07-26 10:50:28 -0700

    more programs

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	t = malloc(width * sizeof(int));
+	t = malloc(width * sizeof(int *));
 	if (t == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
