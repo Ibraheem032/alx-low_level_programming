b7450de4a96837b7dec0d9d54c929cfc273f8a2e
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-08-01 10:02:24 -0700

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,9 +15,9 @@ dog_t *new_dog(char *name, float age, char *owner)
        	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
 	s = &d;
+	s->name = name;
+	s->age = age;
+	s->owner = owner;
 	return (s);
 }
