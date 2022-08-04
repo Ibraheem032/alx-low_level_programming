1842f142610f668649872d5f47ac8ca6a1c9d98e
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-08-03 21:10:42 -0700

    Correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,12 @@
 /**
  * print_name - Prints name
  * @name: Pointer to the name
- * @f: Function pointer 
+ * @f: Function pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
+	{
 		f(name);
+	}
 }
