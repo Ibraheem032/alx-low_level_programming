65dd52ed8ef38bf64317799d2ed8a7f12f253bae
Ibraheem Abdulwakeel <106752814+Ibraheem032@users.noreply.github.com>
2022-08-19 08:08:49 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,33 @@
 #include "main.h"
-#include <math.h>
+/**
+ * countbits - counts number of bits in an int
+ * @n: number to be worked on
+ * Return: an int
+ */
+int countbits(unsigned long int n)
+{
+        int l;
+
+        l = 0;
+        While (n > 0)
+        {
+                n = (unsigned long int) (n * 0.5);
+                l++;
+        }
+        return (l);
+}
 /**
  * print_binary - Prints the binary representation of a number
  * @n: Numbber to be converted to binary
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	printf("%ld", n & 1);
+        int l, i;
+
+        l = countbits(n);
+        for (i = l; i > 0; i--)
+        {
+                num = (n & (1 << i)) >> i;
+                printf("%ld", num);
+        }
 }
