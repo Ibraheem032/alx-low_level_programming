a69c6ce2981de82aa42bb086bccfe7def03f8a8d
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-08-23 00:12:22 +0000

    Updating programs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 /**
  * read_textfile - Reads a text file and prints it to the the POSIX stdo
  * @filename: Pointer to the name of the file
- * @letter: Number of letters it should read and print
+ * @letters: Number of letters it should read and print
  *
  * Return: The actual number of letters it could read and print
  */
@@ -20,14 +20,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename,O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	count = read(fd, buf, letters);
 	if (count == -1)
 		return (0);
 	close(fd);
-	for (i = 0; i < count; i ++)
+	for (i = 0; i < count; i++)
 		_putchar(buf[i]);
 	free(buf);
 	return (count);
